d457679998d795bae92dea54ac73aa693e9ef422
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-04-10 02:35:26 +0300

    feat: add 04-mine-sweeper.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,66 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matrixWidth = matrix[0].length;
+  const matrixHeight = matrix.length;
+  const newMatrix = [];
+
+  for (let i = 0; i < matrixHeight; i++) {
+    newMatrix[i] = [];
+    for (let j = 0; j < matrixWidth; j++) {
+      newMatrix[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i < matrixHeight; i++) {
+    for (let j = 0; j < matrixWidth; j++) {
+      if (matrix[i][j] === true) {
+        newMatrix[i][j] = true;
+
+        if (newMatrix[i - 1]) {
+          if (typeof newMatrix[i - 1][j - 1] !== 'undefined' && newMatrix[i - 1][j - 1] !== true) {
+            newMatrix[i - 1][j - 1] += 1;
+          }
+          if (typeof newMatrix[i - 1][j] !== 'undefined' && newMatrix[i - 1][j] !== true) {
+            newMatrix[i - 1][j] += 1;
+          }
+          if (typeof newMatrix[i - 1][j + 1] !== 'undefined' && newMatrix[i - 1][j + 1] !== true) {
+            newMatrix[i - 1][j + 1] += 1;
+          }
+        }
+
+        if (newMatrix[i]) {
+          if (typeof newMatrix[i][j - 1] !== 'undefined' && newMatrix[i][j - 1] !== true) {
+            newMatrix[i][j - 1] += 1;
+          }
+          if (typeof newMatrix[i][j + 1] !== 'undefined' && newMatrix[i][j + 1] !== true) {
+            newMatrix[i][j + 1] += 1;
+          }
+        }
+
+        if (newMatrix[i + 1]) {
+          if (typeof newMatrix[i + 1][j - 1] !== 'undefined' && newMatrix[i + 1][j - 1] !== true) {
+            newMatrix[i + 1][j - 1] += 1;
+          }
+          if (typeof newMatrix[i + 1][j] !== 'undefined' && newMatrix[i + 1][j] !== true) {
+            newMatrix[i + 1][j] += 1;
+          }
+          if (typeof newMatrix[i + 1][j + 1] !== 'undefined' && newMatrix[i + 1][j + 1] !== true) {
+            newMatrix[i + 1][j + 1] += 1;
+          }
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < matrixHeight; i++) {
+    for (let j = 0; j < matrixWidth; j++) {
+      if (newMatrix[i][j] === true) newMatrix[i][j] = 1;
+    }
+  }
+
+  return newMatrix;
 }
 
 module.exports = minesweeper;

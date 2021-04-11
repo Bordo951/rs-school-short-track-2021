da745b6ee1e6eefcf1d60753f57fae0ee1ddbbe2
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-04-11 22:28:57 +0300

    feat: add 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+
+  for (let i = 0, counter = 1; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else {
+      if (counter !== 1) {
+        result += counter + str[i];
+      } else {
+        result += str[i];
+      }
+      counter = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
